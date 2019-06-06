72a23e19f8d8c29a066fdfc054e1fd80b04cc2bd
leilaes <leila.eskandari@hotmail.com>
2019-06-06 15:04:39 +1200

    Add circle package

---
+++ circle.go
@@ -0,0 +1,13 @@
+package circle
+
+type Circle struct {
+	Radius float64
+}
+
+func (c *Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * 3.14 * c.Radius
+}
